70c649cdebee1fe7571331395addec2f25509fc6
Moustafa Mohamed <musta7188@hotmail.it>
2019-12-04 18:17:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function happyHolidayTo(holiday, name){
 
 function holidayCountdown(holiday, days){
 
-  return `It's ${days} days until ${holiday}`
+  return `It's ${days} days until ${holiday}!`
 
 }
